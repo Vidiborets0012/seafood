b5d58eb3d5c8d8eeec1850bc9e083439825aa068
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-01-25 18:42:15 +0200

    add header styles

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    
+    $('.header-slider').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/icon-prev.svg" alt="icon-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/icon-next.svg" alt="icon-next"></button>',
+        fade: true
+    });
+});
