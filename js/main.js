99b59bb1e7e9807982aa0beedc5fea3d71b4c4fa
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-01-26 20:30:48 +0200

    connect the slider, add styles to the product-name block of the product section

--- js/main.js
+++ js/main.js
@@ -6,4 +6,21 @@ $(function () {
         nextArrow: '<button type="button" class="slick-next"><img src="images/icon-next.svg" alt="icon-next"></button>',
         fade: true
     });
+
+    $('.product-name').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        asNavFor: '.product-content',
+        prevArrow: '<button type="button" class="slick-product-prev"><img src="images/product-icon-prev.svg" alt="icon-prev"></button>',
+        nextArrow: '<button type="button" class="slick-product-next"><img src="images/product-icon-next.svg" alt="icon-next"></button>',
+        vertical: true
+        
+    });
+    $('.product-content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.product-name',
+        fade: true        
+    });
 });
